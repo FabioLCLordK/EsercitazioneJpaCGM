5758ccb280aa0e0e999fe0d0af48d42075eeb9db
LordKazor <LordKazor@KazorCaverna2.mshome.net>
2023-03-10 10:21:27 +0100

    Rev 1.0.1

--- src/main/java/teoryPersitence/Test.java
+++ src/main/java/teoryPersitence/Test.java
@@ -1,6 +1,8 @@
 package teoryPersitence;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Test {
@@ -9,6 +11,20 @@ public class Test {
         //inserisco ciò che ho inserito nel mio persistence.xml -> "mysql-pu"S
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql-pu");
         
+        
+        //per ogni thread un entity manager e un e. transaction
+        EntityManager  entityManager= factory.createEntityManager();       
+        EntityTransaction transaction= entityManager.getTransaction();
+        
+        transaction.begin();
+            
+        
+        //operazioni da inviare
+        //operazione 1
+        //operazione 2
+        //operazione 3
+        transaction.commit();
+        entityManager.clear();
         
         factory.close();
     }
